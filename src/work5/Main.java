8faecc7ce6ba13db51313b7a4f0a7adefacd1858
Olena Kyrpych <umm.marat@gmail.com>
2022-11-21 16:06:08 +0200

    Завдання 5, ДЗ 2, Java Essential

---
+++ src/work5/Main.java
@@ -0,0 +1,8 @@
+package work5;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MyArea.areaOfCircle(15);
+    }
+}
---
+++ src/work5/MyArea.java
@@ -0,0 +1,10 @@
+package work5;
+
+public class MyArea {
+    private static final float PI = 3.14F;
+
+    static void areaOfCircle(int radius){
+
+        System.out.println(PI * Math.pow(radius, 2));
+    }
+}
